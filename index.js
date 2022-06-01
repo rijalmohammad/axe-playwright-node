0fe538cf62b2e5b5b29af56d52a407e3c2bbc9ab
rijalmohammad <mohammad.rijal13@gmail.com>
2022-06-01 16:03:21 +0300

    change name to webpageName

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/scan", async (req, res) => {
       .analyze();
 
     const basicWebpage = {
-      name: title,
+      webpageName: title,
       url: results.url,
       scanTime: results.timestamp,
       issues: [],
